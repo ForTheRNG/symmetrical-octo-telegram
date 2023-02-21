180c0b61befce37f683afbb45e3033e102c0ab59
ForTheRNG <radu.andrei.sirghe@gmail.com>
2023-02-22 01:53:50 +0200

    last update @ Wed 02/22/2023

--- start.bat
+++ start.bat
@@ -1,5 +1,17 @@
-@echo off
-cd /d C:\Users\Matei\Desktop\Server
+rem @echo off
+
+rem figure out ram allocated
+cd /d %~dp0
 set RAM=4096
 if defined %~1 set RAM=%~1
-java -Xmx%RAM%M -Xms%RAM%M -jar minecraft_server.1.19.3.jar
+
+rem start server
+rem java -Xmx%RAM%M -Xms%RAM%M -jar minecraft_server.1.19.3.jar
+
+rem check for git, maybe update
+git --version
+if ERRORLEVEL 0 (
+	git add *
+	git commit -m "last update @ %DATE%"
+	git push backup master
+)
