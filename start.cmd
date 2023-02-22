7692e0ed62799c87c3c02e9b1d3ecc5f574a5ba9
ForTheRNG <radu.andrei.sirghe@gmail.com>
2023-02-22 23:47:04 +0200

    last update @ Wed 02/22/2023

--- start.cmd
+++ start.cmd
@@ -1,11 +1,12 @@
 rem @echo off
 
 rem elevate if not done already
+set PWSH=powershell -c Start-Process -Wait -Verb RunAs -WindowStyle Maximized
 net session 1>NUL 2>&1
 if "%ERRORLEVEL%"=="0" goto :startup
-echo powershell -c Start-Process -Wait -Verb RunAs -WindowStyle Maximized '"%~f0" %*'
-powershell -c Start-Process -Wait -Verb RunAs -WindowStyle Maximized -FilePath "%~f0" -Args '%*'
-exit /b %ERRORLEVEL%
+echo %PWSH% -FilePath "%~f0"
+if "%~1"=="" (%PWSH% -FilePath "%~0") else (%PWSH% -FilePath "%~0" -Args '%*')
+goto :endoffile
 
 :startup
 rem figure out ram allocated
@@ -26,3 +27,6 @@ if ERRORLEVEL 0 (
 	git commit -m "last update @ %DATE%"
 	git push backup master
 )
+
+:endoffile
+exit /b %ERRORLEVEL%
